82e1052eb38506efdeaf05e2ad7408378545ed9e
tomoron <tomoron326@outlook.jp>
2021-05-03 18:29:59 +0900

    add:product_show

--- backend/app/controllers/api/products_controller.rb
+++ backend/app/controllers/api/products_controller.rb
@@ -1,9 +1,16 @@
 module Api
   class ProductsController < ApplicationController
     def index
-      @products = Product.all
+      products = Product.all
       render json: {
-        products: @products
+        products: products
+      }, status: :ok
+    end
+
+    def show
+      product = Product.find(params[:id])
+      render json: {
+        product: product
       }, status: :ok
     end
   end
--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -5,7 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-3.times do |i|
+12.times do |i|
   Product.create!(
     name: i,
     price: i,
