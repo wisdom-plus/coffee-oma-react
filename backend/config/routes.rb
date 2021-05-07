bb68a6d3aa610a61007989f595bb11bec4b7bd2c
tomoron <tomoron326@outlook.jp>
2021-05-07 10:19:14 +0900

    config:devise_auth

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  mount_devise_token_auth_for 'User', at: 'auth', controller: { registrations: 'auth/registrations' }
   namespace :api do
     resources :products
   end
