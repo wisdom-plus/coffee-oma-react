895781a2f092bda3c48e17be53833dd2117bb0e8
tomoron <tomoron326@outlook.jp>
2021-05-07 14:18:14 +0900

    add:confirm_user

--- backend/config/application.rb
+++ backend/config/application.rb
@@ -31,10 +31,24 @@ module Backend
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.i18n.default_locale =:ja
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.session_store :cookie_store, key: '_interslice_session'
+    config.middleware.use ActionDispatch::Cookies # Required for all session management
+    config.middleware.use ActionDispatch::Session::CookieStore, config.session_options
+    config.middleware.use ActionDispatch::Flash
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*',
+                 :headers => :any,
+                 :expose => ['access-token', 'expiry', 'token-type', 'uid', 'client'],
+                 :methods => [:get, :post, :options, :delete, :put]
+      end
+    end
   end
 end
--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
-  mount_devise_token_auth_for 'User', at: 'auth', controller: { registrations: 'auth/registrations' }
+
   namespace :api do
     resources :products
+    mount_devise_token_auth_for 'User', at: 'auth', controller: { registrations: 'api/auth/registrations' }
   end
+
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
