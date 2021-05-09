96e3f5c487cd24f56c194447d3d8b2694004deed
tomoron <tomoron326@outlook.jp>
2021-05-09 10:00:20 +0900

    add:menu_sign_up

--- frontend/src/components/organisms/Topmenu.tsx
+++ frontend/src/components/organisms/Topmenu.tsx
@@ -1,11 +1,19 @@
 import { FC } from 'react';
 import { Menu, Container } from 'semantic-ui-react';
 import Menulogo from 'components/atoms/Menulogo';
+import { Link } from 'react-router-dom';
 
 const Topmenu: FC = () => (
   <Menu fixed="top" inverted color="teal">
     <Container>
       <Menulogo />
+      <Menu.Item
+        header
+        as={Link}
+        to="/sign_up"
+        content="新規登録"
+        position="right"
+      />
     </Container>
   </Menu>
 );
