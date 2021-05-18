e1220324888a0fc990383564c2e773ac6eedbc4f
tomoron <tomoron326@outlook.jp>
2021-05-18 16:56:05 +0900

    add:like_controller

--- backend/app/controllers/api/likes_controller.rb
+++ backend/app/controllers/api/likes_controller.rb
@@ -1,7 +1,21 @@
 module Api
   class LikesController < ApplicationController
-    def create; end
+    def create
+      like = Like.new(params[:product_id])
+      if like.save
+        render json: { status: 'OK' }, status: :created
+      else
+        render json: { status: 'Failure' }, status: :internal_server_error
+      end
+    end
 
-    def destroy; end
+    def destroy
+      like = Like.find_by(id:params[:product_id])
+      if like.destroy
+        render json: { status: 'OK' }, status: :created
+      else
+        render json: { status: 'Failure' }, status: :internal_server_error
+      end
+    end
   end
 end
