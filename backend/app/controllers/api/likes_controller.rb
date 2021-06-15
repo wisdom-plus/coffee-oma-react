7645cd6e8060eb63a8f5cc525f4bdf33b43e4231
tomoron <tomoron326@outlook.jp>
2021-06-16 00:16:59 +0900

    fix:rubocop

--- backend/app/controllers/api/likes_controller.rb
+++ backend/app/controllers/api/likes_controller.rb
@@ -33,7 +33,7 @@ module Api
       product = Product.find_by(id: params[:product_id])
       if product
         liked = api_user_signed_in? && product.likes.exists?(user_id: current_api_user.id)
-          render json: {liked: liked ,count: product.likes.count }, status: :ok
+        render json: { liked: liked, count: product.likes.count }, status: :ok
       else
         render status: :internal_server_error
       end
