3240ffb9e26f5225d2370d02f5409f206bd283b0
tomoron <tomoron326@outlook.jp>
2021-11-07 10:46:16 +0900

    npm_install

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -122,7 +122,6 @@ module.exports = {
       },
     ],
     'react/react-in-jsx-scope': 'off',
-    'react/require-default-props': [0],
     'react-hooks/rules-of-hooks': 'error',
     'react-hooks/exhaustive-deps': [
       'warn',
