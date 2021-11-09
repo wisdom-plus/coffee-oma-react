93cebcafc5002e2180a05cc1f80460a968ab89da
tomoron <tomoron326@outlook.jp>
2021-11-09 16:30:39 +0900

    fix:delete

---
+++ frontend/src/components/templates/ProductNew.tsx
@@ -0,0 +1,17 @@
+import { FC } from 'react';
+import { Header } from 'semantic-ui-react';
+import Newform from 'container/EnhancedNewForm';
+
+const ProductNew: FC = () => (
+  <>
+    <Header
+      as="h1"
+      content="アイテムを登録する"
+      textAlign="center"
+      style={{ marginBottom: '1rem' }}
+    />
+    <Newform />
+  </>
+);
+
+export default ProductNew;
