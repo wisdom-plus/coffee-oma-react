876ed2beab67c62f1368e5fa26fabd384ac47ed6
tomoron <tomoron326@outlook.jp>
2021-11-14 18:50:07 +0900

    add:review_action

--- backend/app/controllers/api/v1/review_controller.rb
+++ backend/app/controllers/api/v1/review_controller.rb
@@ -1,10 +1,39 @@
-class ReviewController < ApplicationController
-  def create
-  end
+module Api
+  module V1
+    class ReviewController < ApplicationController
+      before_action :authenticate_api_v1_user!, only: %i[create destroy]
 
-  def destory
-  end
+      def create
+        review = current_api_v1_user.reviews.new(reivew_params)
+        if review.save
+          render status: :created
+        else
+          render status: :not_found
+        end
+      end
 
-  def exists
+      def destory
+        review = Review.find_by(id: params[:id])
+        if review.destroy
+          render status: :ok
+        else
+          render status: :not_found
+        end
+      end
+
+      def exists
+        reviews = Review.get_reviews(params[:id])
+        if review
+          render json: {reviews: reviews},status: :ok
+        else
+          render status: :not_found
+        end
+      end
+
+      private
+        def review_params
+          params.require(:review).permit(:title,:content,:rate)
+        end
+    end
   end
 end
--- backend/app/models/review.rb
+++ backend/app/models/review.rb
@@ -2,5 +2,5 @@ class Review < ApplicationRecord
 
   belongs_to :product
   belongs_to :user
-
+  scope :get_reviews ,->(id) {where("product_id = ?", id)}
 end
