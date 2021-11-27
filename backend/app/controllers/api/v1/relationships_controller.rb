d97351d7711e08a8464b970d906240f3eee3e25e
tomoron <tomoron326@outlook.jp>
2021-11-27 17:48:54 +0900

    fix:rubocop

--- backend/app/controllers/api/v1/relationships_controller.rb
+++ backend/app/controllers/api/v1/relationships_controller.rb
@@ -25,12 +25,12 @@ module Api
       def exists
         user = User.find_by(id: params[:follow_id])
         if user
-          follow =  if api_v1_user_signed_in?
-                        current_api_v1_user.following?(user)
-                    else
-                      false
-                    end
-          render json: {follow: follow} , status: :ok
+          follow = if api_v1_user_signed_in?
+                     current_api_v1_user.following?(user)
+                   else
+                     false
+                   end
+          render json: { follow: follow }, status: :ok
         else
           render status: :not_found
         end
