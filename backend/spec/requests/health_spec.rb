0ae41fbead5324e1cffd89b3003978867147ae76
tomoron <tomoron326@outlook.jp>
2022-01-30 22:56:47 +0900

    add:spec_health

--- backend/spec/requests/health_spec.rb
+++ backend/spec/requests/health_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe 'Healths', type: :request do
   describe 'GET /index' do
     it 'returns http success' do
-      get '/health/index'
+      get api_v1_health_index_path
       expect(response).to have_http_status(:success)
     end
   end
