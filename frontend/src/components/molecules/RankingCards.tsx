b918cebfc0a8bfa7153b788c2dd50e541a382b93
tomoron <tomoron326@outlook.jp>
2022-04-27 16:50:19 +0900

    pref: mv Card.Group

--- frontend/src/components/molecules/RankingCards.tsx
+++ frontend/src/components/molecules/RankingCards.tsx
@@ -1,31 +1,31 @@
 import { FC, RefObject } from 'react';
-import { Card, Segment, Ref } from 'semantic-ui-react';
+import { Segment, Ref } from 'semantic-ui-react';
 import RankingCard from 'components/atoms/RankingCard';
 import { Product } from 'model/index';
 import { motion } from 'framer-motion';
 import { list } from 'constant/index';
 import RailTop from 'components/atoms/RailTop';
 
-const RankingCards: FC<{ state: Product[]; Railref: RefObject<HTMLElement> }> =
-  ({ state = [], Railref }) => (
-    <Ref innerRef={Railref}>
-      <Segment>
-        <motion.div initial="hidden" animate="visible" variants={list}>
-          <motion.div
-            className="thumbnails"
-            initial="initial"
-            animate="enter"
-            exit="exit"
-            variants={{ exit: { transition: { staggerChildren: 0.1 } } }}
-          >
-            <Card.Group itemsPerRow={3} stackable centered>
-              <RankingCard products={state} />
-            </Card.Group>
-          </motion.div>
+const RankingCards: FC<{
+  state: Product[];
+  Railref: RefObject<HTMLElement>;
+}> = ({ state = [], Railref }) => (
+  <Ref innerRef={Railref}>
+    <Segment>
+      <motion.div initial="hidden" animate="visible" variants={list}>
+        <motion.div
+          className="thumbnails"
+          initial="initial"
+          animate="enter"
+          exit="exit"
+          variants={{ exit: { transition: { staggerChildren: 0.1 } } }}
+        >
+          <RankingCard products={state} />
         </motion.div>
-        <RailTop Railref={Railref} />
-      </Segment>
-    </Ref>
-  );
+      </motion.div>
+      <RailTop Railref={Railref} />
+    </Segment>
+  </Ref>
+);
 
 export default RankingCards;
